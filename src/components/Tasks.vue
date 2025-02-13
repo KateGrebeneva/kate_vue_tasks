6d722cd3c4ed0f9a14daaff60baa33a4d6ba70b3
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2025-02-13 22:17:58 +0300

    task#3

---
+++ src/components/Tasks.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="greeting">
+    Hey! My name is Catherine, hug you!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tasks',
+}
+</script>
+
+<style scoped>
+.greeting {
+  color: #D5006D; /* Темно-розовый цвет */
+  font-family: 'Arial', sans-serif; /* Шрифт */
+  font-size: 24px; /* Размер шрифта */
+  text-align: center; /* Выравнивание по центру */
+  padding: 20px; /* Отступы */
+  border: 2px solid #D5006D; /* Рамка */
+  border-radius: 10px; /* Скругление углов */
+  background-color: #F8BBD0; /* Светлый фон */
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1); /* Тень */
+}
+</style>
+
