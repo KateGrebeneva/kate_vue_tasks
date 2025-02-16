b1bb2a4128b70658e968cc8d2e0cbc17faff5a1b
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2025-02-16 22:34:36 +0300

    task#6

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="link-container">
-    <a :href="href" class="styled-link">{{ text }}</a>
+  <div class="container">
+    <h1 class="styled-text">Задание №1: Квадрат числа</h1>
+    <p class="result">{{ num * num }}</p>
+
+    <h1 class="styled-text">Задание №2: Сумма чисел</h1>
+    <p class="result">{{ num1 + num2 + num3 }}</p>
   </div>
 </template>
 
@@ -8,30 +12,42 @@
 export default {
   data() {
     return {
-      text: 'Adventures of the Gummi Bears',
-      href: 'https://www.kinopoisk.ru/series/81023/',
+      num: 5,
+      num1: 1,
+      num2: 2,
+      num3: 3,
     }
   }
 }
 </script>
 
 <style scoped>
-.link-container {
-  display: flex; 
-  justify-content: center; 
-  margin-top: 20px; 
+.container {
+  text-align: center; 
+  margin-top: 50px; 
 }
 
-.styled-link {
-  color: #D5006D;
+.styled-text {
+  color: #D5006D; 
   font-family: 'Arial', sans-serif; 
-  font-size: 24px;
-  text-decoration: none; 
-  transition: color 0.3s ease; 
+  font-size: 28px; 
+  margin-bottom: 20px; 
 }
 
-.styled-link:hover {
-  color: #FF4081; 
-  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
+.result {
+  color:rgb(158, 0, 71); 
+  background-color:rgba(242, 12, 131, 0.55); 
+  font-family: 'Arial', sans-serif; 
+  font-size: 24px; 
+  padding: 10px; 
+  border: 2px solid #D5006D; 
+  border-radius: 8px; 
+  display: inline-block; 
+  transition: background-color 0.3s ease; 
+}
+
+.result:hover {
+  background-color: rgba(213, 0, 109, 0.8); 
 }
 </style>
+
