1a7c13786709ac920f4c6a162a725ea1bf5eb060
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2025-02-16 22:24:57 +0300

    task#5

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,7 +1,6 @@
 <template>
-  <div>
-    <div class="user-name">{{ name }}</div>
-    <div class="user-surname">{{ surn }}</div>
+  <div class="link-container">
+    <a :href="href" class="styled-link">{{ text }}</a>
   </div>
 </template>
 
@@ -9,19 +8,30 @@
 export default {
   data() {
     return {
-      name: 'Emma',
-      surn: 'Swan',
+      text: 'Adventures of the Gummi Bears',
+      href: 'https://www.kinopoisk.ru/series/81023/',
     }
   }
 }
 </script>
 
 <style scoped>
-.user-name, .user-surname {
-  color: #D5006D; /* Темно-розовый цвет */
-  font-family: 'Arial', sans-serif; /* Шрифт */
-  font-size: 20px; /* Размер шрифта */
-  margin: 10px 0; /* Отступы */
-  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.3); /* Тень текста */
+.link-container {
+  display: flex; 
+  justify-content: center; 
+  margin-top: 20px; 
+}
+
+.styled-link {
+  color: #D5006D;
+  font-family: 'Arial', sans-serif; 
+  font-size: 24px;
+  text-decoration: none; 
+  transition: color 0.3s ease; 
+}
+
+.styled-link:hover {
+  color: #FF4081; 
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
 }
 </style>
