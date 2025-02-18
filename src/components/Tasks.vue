90ddd78a7cd331f30a1502e1ae847f7037b030a1
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2025-02-18 18:19:07 +0300

    task#21

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container">
     <div v-for="(item, index) in items" :key="index" class="item">
-      <button @click="toggle(index)" class="styled-button">Toggle Paragraph {{ index + 1 }}</button>
+      <button @click="toggle(index)" class="styled-button">
+        {{ item.visible ? 'Скрыть' : 'Показать' }} {{ item.title }}
+      </button>
       <p v-if="item.visible" class="styled-text">{{ item.text }}</p>
     </div>
   </div>
@@ -12,9 +14,9 @@ export default {
   data() {
     return {
       items: [
-        { text: 'Румпельштильцхен — Тёмный, сильнейший из волшебников Зачарованного Леса.', visible: false },
-        { text: 'Очень хитрый и проницательный, загадочный и увёртливый маг.', visible: false },
-        { text: 'Является учителем трёх великих колдуний семейства Миллс — Коры, Реджины и Зелины.', visible: false },
+        { title: 'Принцесса Ариэль', text: 'Ариэль — русалочка, которая мечтает стать человеком.', visible: false },
+        { title: 'Принцесса Белль', text: 'Белль — умная и независимая девушка, которая любит читать книги.', visible: false },
+        { title: 'Принцесса Жасмин', text: 'Жасмин — смелая принцесса, которая стремится к свободе и приключениям.', visible: false },
       ],
     };
   },
@@ -45,18 +47,18 @@ export default {
 }
 
 .styled-button {
-  background-color: #d5006d; 
+  background-color: #d5006d;
   color: white; 
   border: none; 
   border-radius: 8px; 
   padding: 12px 24px; 
   font-size: 18px; 
   cursor: pointer; 
-  transition: background-color 0.3s, transform 0.2s;
+  transition: background-color 0.3s, transform 0.2s; 
 }
 
 .styled-button:hover {
-  background-color: #c51162;
+  background-color: #c51162; 
 }
 
 .styled-button:active {
