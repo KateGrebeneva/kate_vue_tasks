ea3f719ea59e3d3fc8bfe4f02a4c221c70927ba8
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2025-02-18 18:32:07 +0300

    task#24

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,12 +1,8 @@
 <template>
   <div class="container">
-    <p v-if="day === 'Понедельник'" class="day-message">Понедельник</p>
-    <p v-if="day === 'Вторник'" class="day-message">Вторник</p>
-    <p v-if="day === 'Среда'" class="day-message">Среда</p>
-    <p v-if="day === 'Четверг'" class="day-message">Четверг</p>
-    <p v-if="day === 'Пятница'" class="day-message">Пятница</p>
-    <p v-if="day === 'Суббота'" class="day-message">Суббота</p>
-    <p v-if="day === 'Воскресенье'" class="day-message">Воскресенье</p>
+    <p v-if="age < 18" class="text">Подросток</p>
+    <p v-else-if="age >= 18 && age <= 25" class="text">Девушка</p>
+    <p v-else class="text">Женщина</p>
   </div>
 </template>
 
@@ -14,7 +10,7 @@
 export default {
   data() {
     return {
-      day: 'Вторник', 
+      age: 18, 
     };
   },
 };
@@ -24,20 +20,15 @@ export default {
 .container {
   text-align: center;
   margin-top: 50px;
-}
-
-.day-message {
-  color: white;
-  background-color: #d5006d; /* Темно-розовый фон */
+  font-size: 24px;
+  background-color: #ff69b4; 
   padding: 20px;
   border-radius: 10px;
-  font-size: 24px;
-  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
-  transition: transform 0.3s, background-color 0.3s;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); 
 }
 
-.day-message:hover {
-  transform: scale(1.05);
-  background-color: #c51162; /* Более светлый оттенок при наведении */
+.text {
+  color: white;
+  font-weight: bold; 
 }
 </style>
