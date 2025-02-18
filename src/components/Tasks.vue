3659966db2ae986503c2038379ce4e1eebc0bb44
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2025-02-18 17:47:18 +0300

    task#16_2

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="container">
-    <h1 class="title">Task#16 №1</h1>
-    <a href="https://once-upon-a-time.fandom.com/ru/wiki/%D0%9E%D0%B4%D0%BD%D0%B0%D0%B6%D0%B4%D1%8B_%D0%B2_%D1%81%D0%BA%D0%B0%D0%B7%D0%BA%D0%B5" @click.prevent="handleClick" class="link-button">Кликни меня!</a>
+    <button @click="handleClick" :disabled="isClicked" class="click-button">
+      {{ isClicked ? 'Клик выполнен' : 'Нажми на меня' }}
+    </button>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      isClicked: false // Флаг, отслеживающий, был ли клик
+    };
+  },
   methods: {
-    handleClick(event) {
-      console.log('Ссылка была нажата, но переход не произошел');
+    handleClick() {
+      if (!this.isClicked) {
+        console.log('Первый клик по кнопке!');
+        this.isClicked = true; // Устанавливаем флаг в true после первого клика
+      }
     }
   }
 };
@@ -19,32 +28,20 @@ export default {
 .container {
   text-align: center;
   padding: 50px;
-  background-color: rgb(248, 165, 213); 
-  border-radius: 10px;
-  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.1);
 }
 
-.title {
-  font-size: 2em;
-  color: #d5006d; 
-}
-
-.link-button {
+.click-button {
   padding: 15px 25px;
   border: none;
   border-radius: 5px;
-  cursor: pointer;
-  background-color: rgb(208, 47, 130); 
+  background-color: rgb(208, 47, 130);
   color: white;
   font-size: 1em;
-  margin: 10px;
-  text-decoration: none; /* Убираем подчеркивание */
-  display: inline-block; /* Чтобы кнопка выглядела как кнопка */
-  transition: background-color 0.3s, transform 0.2s; 
+  cursor: pointer;
 }
 
-.link-button:hover {
-  background-color: #c51162; 
-  transform: scale(1.05); 
+.click-button:disabled {
+  background-color: rgba(208, 47, 130, 0.5);
+  cursor: not-allowed;
 }
 </style>
