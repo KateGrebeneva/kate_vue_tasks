cf5af4b9a9962d8d602776c9eca9b3b2af05e539
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2025-02-21 00:20:13 +0300

    task#43_1

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,11 +1,38 @@
 <template>
-  <p :style="{ color: 'green', backgroundColor: 'yellow', fontWeight: 'bold', fontStyle: 'italic'  }">
-    Это пример текста, окрашенного в зеленый цвет на желтом фоне.
-  </p>
+  <div class="task-1">
+    <h2>Задание 1</h2>
+    <input v-model="message" placeholder="Введите текст">
+    <p>{{ message }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'ExampleComponent'
+  data() {
+    return {
+      message: 'hello',
+    };
+  },
 };
 </script>
+
+<style scoped>
+.task-1 {
+  background-color:rgb(255, 155, 205);
+  padding: 20px;
+  border-radius: 10px;
+  color: white;
+}
+input {
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+  margin-top: 10px;
+  color:rgb(202, 0, 101);
+}
+p, h2 {
+  font-size: 18px;
+  margin-top: 10px;
+  color:rgb(202, 0, 101);
+}
+</style>
