3e4e2026451e32804572dcc4fececbe037f44132
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2025-02-22 14:33:07 +0300

    task#52

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="container">
-    <h2>Блокировка элементов в Vue</h2>
+    <h2>Модификаторы клавиш в Vue</h2>
 
+    <!-- Задание №1 -->
     <div class="task" id="task-1">
-      <h3>Задание №1</h3>
-      <input type="text" v-bind:disabled="isInputDisabled" placeholder="Введите текст" />
-      <button @click="toggleInput">Toggle Input</button>
+      <h3>Задание №1: Ввод текста и вывод при нажатии Enter</h3>
+      <input type="text" v-model="inputText" @keyup.enter="showText" placeholder="Введите текст и нажмите Enter" />
+      <p class="output">{{ outputText }}</p>
     </div>
 
+    <!-- Задание №2 -->
     <div class="task" id="task-2">
-      <h3>Задание №2</h3>
-      <input type="checkbox" v-model="isCheckboxChecked" /> Разрешить ввод
-      <input type="text" v-bind:disabled="!isCheckboxChecked" placeholder="Введите текст" />
+      <h3>Задание №2: Вывод текста при нажатии Ctrl и клике на ссылку</h3>
+      <a href="#" @click.prevent="handleCtrlClick">Нажмите Ctrl + клик на меня</a>
+      <p class="output">{{ ctrlOutput }}</p>
+    </div>
+
+    <!-- Задание №3 -->
+    <div class="task" id="task-3">
+      <h3>Задание №3: Вывод текста в зависимости от кнопки мыши</h3>
+      <a href="#" @mousedown="handleMouseDown">Нажмите на меня</a>
+      <p class="output">{{ mouseOutput }}</p>
     </div>
   </div>
 </template>
@@ -20,13 +29,30 @@
 export default {
   data() {
     return {
-      isInputDisabled: true,
-      isCheckboxChecked: false,
+      inputText: '',
+      outputText: '',
+      ctrlOutput: '',
+      mouseOutput: '',
     };
   },
   methods: {
-    toggleInput() {
-      this.isInputDisabled = !this.isInputDisabled;
+    showText() {
+      this.outputText = this.inputText;
+      this.inputText = ''; // Очистка инпута после ввода
+    },
+    handleCtrlClick(event) {
+      if (event.ctrlKey) {
+        this.ctrlOutput = 'Вы нажали Ctrl + клик!';
+      }
+    },
+    handleMouseDown(event) {
+      if (event.button === 0) {
+        this.mouseOutput = 'left';
+      } else if (event.button === 1) {
+        this.mouseOutput = 'middle';
+      } else if (event.button === 2) {
+        this.mouseOutput = 'right';
+      }
     },
   },
 };
@@ -34,17 +60,17 @@ export default {
 
 <style scoped>
 .container {
-  background-color:rgb(253, 191, 221);
-  border: 2px solidrgb(255, 101, 178);
+  background-color:rgb(247, 184, 214);
+  border: 2px solidrgb(252, 112, 182);
   border-radius: 10px;
   padding: 20px;
-  max-width: 400px;
+  max-width: 500px;
   margin: auto;
 }
 
 .task {
-  background-color:rgb(255, 222, 233);
-  border: 1px solid #ff66b2;
+  background-color:rgb(255, 211, 226);
+  border: 1px solidrgb(247, 82, 165);
   border-radius: 5px;
   padding: 15px;
   margin-bottom: 20px;
@@ -52,35 +78,32 @@ export default {
 
 h2 {
   text-align: center;
-  color:rgb(214, 0, 107);
+  color:rgb(193, 0, 96);
 }
 
 h3 {
   color: #cc0066;
 }
 
-button {
-  background-color:rgb(249, 82, 166);
-  color: white;
-  border: none;
-  border-radius: 5px;
-  padding: 10px;
-  cursor: pointer;
-}
-
-button:hover {
-  background-color: #ff3399;
-}
-
 input[type="text"] {
   width: calc(100% - 22px);
   padding: 10px;
   margin-top: 10px;
-  border: 1px solidrgb(207, 29, 118);
+  border: 1px solidrgb(245, 68, 157);
   border-radius: 5px;
 }
 
-input[type="checkbox"] {
-  margin-right: 10px;
+a {
+  color:rgb(234, 15, 124);
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+.output {
+  margin-top: 10px;
+  font-weight: bold;
 }
 </style>
