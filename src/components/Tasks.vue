96483cc9f22f611b72aa60f8e55f4a79e3e52688
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2025-02-22 14:37:44 +0300

    task#53

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,26 +1,23 @@
 <template>
   <div class="container">
-    <h2>Модификаторы клавиш в Vue</h2>
-
     <!-- Задание №1 -->
     <div class="task" id="task-1">
-      <h3>Задание №1: Ввод текста и вывод при нажатии Enter</h3>
-      <input type="text" v-model="inputText" @keyup.enter="showText" placeholder="Введите текст и нажмите Enter" />
-      <p class="output">{{ outputText }}</p>
+      <h3>Задание №1: Добавление элемента в конец списка</h3>
+      <input v-model="newItem" placeholder="Введите новый элемент" />
+      <button @click="addItem">Добавить</button>
+      <ul>
+        <li v-for="(item, index) in items" :key="index">{{ item }}</li>
+      </ul>
     </div>
 
     <!-- Задание №2 -->
     <div class="task" id="task-2">
-      <h3>Задание №2: Вывод текста при нажатии Ctrl и клике на ссылку</h3>
-      <a href="#" @click.prevent="handleCtrlClick">Нажмите Ctrl + клик на меня</a>
-      <p class="output">{{ ctrlOutput }}</p>
-    </div>
-
-    <!-- Задание №3 -->
-    <div class="task" id="task-3">
-      <h3>Задание №3: Вывод текста в зависимости от кнопки мыши</h3>
-      <a href="#" @mousedown="handleMouseDown">Нажмите на меня</a>
-      <p class="output">{{ mouseOutput }}</p>
+      <h3>Задание №2: Добавление элемента в начало списка</h3>
+      <input v-model="newItemForTask2" placeholder="Введите новый элемент" />
+      <button @click="addItemToStart">Добавить в начало</button>
+      <ul>
+        <li v-for="(item, index) in itemsForTask2" :key="index">{{ item }}</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -29,29 +26,23 @@
 export default {
   data() {
     return {
-      inputText: '',
-      outputText: '',
-      ctrlOutput: '',
-      mouseOutput: '',
+      newItem: '',
+      items: ['Swan', 'Cat', 'Dog'],
+      newItemForTask2: '',
+      itemsForTask2: ['Swan', 'Cat', 'Dog'],
     };
   },
   methods: {
-    showText() {
-      this.outputText = this.inputText;
-      this.inputText = ''; // Очистка инпута после ввода
-    },
-    handleCtrlClick(event) {
-      if (event.ctrlKey) {
-        this.ctrlOutput = 'Вы нажали Ctrl + клик!';
+    addItem() {
+      if (this.newItem) {
+        this.items.push(this.newItem);
+        this.newItem = ''; // Очистка инпута после добавления
       }
     },
-    handleMouseDown(event) {
-      if (event.button === 0) {
-        this.mouseOutput = 'left';
-      } else if (event.button === 1) {
-        this.mouseOutput = 'middle';
-      } else if (event.button === 2) {
-        this.mouseOutput = 'right';
+    addItemToStart() {
+      if (this.newItemForTask2) {
+        this.itemsForTask2.unshift(this.newItemForTask2);
+        this.newItemForTask2 = ''; // Очистка инпута после добавления
       }
     },
   },
@@ -60,17 +51,17 @@ export default {
 
 <style scoped>
 .container {
-  background-color:rgb(247, 184, 214);
-  border: 2px solidrgb(252, 112, 182);
+  background-color:rgb(248, 164, 205);
+  border: 2px solid rgb(254, 64, 159);
   border-radius: 10px;
   padding: 20px;
-  max-width: 500px;
+  max-width: 600px;
   margin: auto;
 }
 
 .task {
-  background-color:rgb(255, 211, 226);
-  border: 1px solidrgb(247, 82, 165);
+  background-color:rgb(250, 199, 216);
+  border: 1px solid rgb(237, 61, 149);
   border-radius: 5px;
   padding: 15px;
   margin-bottom: 20px;
@@ -78,7 +69,7 @@ export default {
 
 h2 {
   text-align: center;
-  color:rgb(193, 0, 96);
+  color: #ff3399;
 }
 
 h3 {
@@ -89,21 +80,32 @@ input[type="text"] {
   width: calc(100% - 22px);
   padding: 10px;
   margin-top: 10px;
-  border: 1px solidrgb(245, 68, 157);
+  border: 1px solid #ff66b2;
   border-radius: 5px;
 }
 
-a {
-  color:rgb(234, 15, 124);
-  text-decoration: none;
+button {
+  background-color: #ff3399;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  padding: 10px;
+  cursor: pointer;
 }
 
-a:hover {
-  text-decoration: underline;
+button:hover {
+  background-color: #cc0066;
 }
 
-.output {
-  margin-top: 10px;
-  font-weight: bold;
+ul {
+  list-style-type: none; 
+  padding-left: 0; 
+}
+
+li {
+  background-color: #fff; 
+  margin: 5px 0; 
+  padding: 10px; 
+  border-radius: 5px; 
 }
 </style>
