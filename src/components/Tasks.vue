4c3d4f950eeaa7367ec0f991498b696916d98c9e
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2025-02-22 14:50:09 +0300

    task#55

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,16 +1,31 @@
 <template>
   <div class="container">
-    <h2>Удаление элементов из списка в Vue</h2>
-
-    <!-- Задание: Удаление элемента из списка -->
-    <div class="task" id="remove-item-task">
-      <h3>Персонажи из сериала "Сотня"</h3>
-      <ul>
-        <li v-for="(item, index) in items" :key="index" class="list-item">
-          {{ item }}
-          <button @click="removeItem(index)" class="remove-button">Удалить</button>
-        </li>
-      </ul>
+    <h2>Работники</h2>
+    
+    <div class="task" id="remove-worker-task">
+      <h3>Задание 2: Удаление работника</h3>
+      <table class="worker-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Имя</th>
+            <th>Зарплата</th>
+            <th>Возраст</th>
+            <th>Действия</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in users" :key="user.id">
+            <td>{{ user.id }}</td>
+            <td>{{ user.name }}</td>
+            <td>{{ user.salary }}</td>
+            <td>{{ user.age }}</td>
+            <td>
+              <button @click="removeWorker(user.id)" class="remove-button">Удалить</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
@@ -19,12 +34,16 @@
 export default {
   data() {
     return {
-      items: ['Беллами', 'Кларк', 'Октавия', 'Джаспер', 'Финн'],
+      users: [
+        { id: 1, name: 'Jaden', salary: 19000, age: 22 },
+        { id: 2, name: 'Vinnie', salary: 2500, age: 23 },
+        { id: 3, name: 'Josh', salary: 3100, age: 25 },
+      ],
     };
   },
   methods: {
-    removeItem(index) {
-      this.items.splice(index, 1);
+    removeWorker(id) {
+      this.users = this.users.filter(user => user.id !== id);
     },
   },
 };
@@ -32,8 +51,8 @@ export default {
 
 <style scoped>
 .container {
-  background-color:rgb(254, 176, 214);
-  border: 2px solid rgb(228, 38, 133);
+  background-color:rgb(255, 163, 208);
+  border: 2px solid rgb(207, 19, 113);
   border-radius: 10px;
   padding: 20px;
   max-width: 600px;
@@ -41,33 +60,32 @@ export default {
 }
 
 .task {
-  background-color:rgb(251, 195, 214);
-  border: 1px solid rgb(228, 40, 134);
+  background-color:rgb(255, 202, 218);
   border-radius: 5px;
-  padding: 15px;
 }
 
 h2 {
   text-align: center;
-  color: #ff3399;
+  color:rgb(199, 15, 107);
 }
 
 h3 {
   color: #cc0066;
 }
 
-ul {
-  list-style-type: none; /* Убираем маркеры */
-  padding-left: 0; /* Убираем отступ слева */
+.worker-table {
+  width: 100%;
+  border-collapse: collapse; /* Убираем двойные границы */
 }
 
-.list-item {
-  background-color: #fff; /* Белый фон для элементов списка */
-  margin: 5px 0; /* Отступы между элементами списка */
+.worker-table th,
+.worker-table td {
+  border: 1px solid rgb(255, 217, 236); /* Розовые границы для ячеек */
   padding: 10px; /* Внутренние отступы */
-  border-radius: 5px; /* Закругленные углы */
-  display: flex; /* Используем flex для выравнивания кнопки */
-  justify-content: space-between; /* Распределяем элементы по краям */
+}
+
+.worker-table th {
+  background-color:rgb(255, 180, 218); /* Цвет фона заголовков */
 }
 
 .remove-button {
@@ -75,8 +93,7 @@ ul {
   color: white;
   border: none;
   border-radius: 5px;
-  padding: 5px 10px;
-  cursor: pointer;
+  padding: 5px;
 }
 
 .remove-button:hover {
