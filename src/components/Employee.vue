8737b005562ad90b01b7c0dca096ce8dc5ee3cac
KateGrebeneva <grebeneva.k.i.23@gmail.com>
2025-02-24 12:47:23 +0300

    task#58

--- src/components/Employee.vue
+++ src/components/Employee.vue
@@ -1,25 +1,22 @@
 <template>
   <div class="employee-container">
     <h2 class="employee-title">Employee Information</h2>
-    <p class="employee-name">{{ name }} - {{ position }}</p>
+    <p class="employee-name">Name: {{ name }}</p>
+    <p class="employee-surname">Surname: {{ surn }}</p>
+    <p class="employee-age">Age: {{ age }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      name: 'Hopper',
-      position: 'Doctor'
-    }
-  }
+  props: ['name', 'surn', 'age']
 }
 </script>
 
 <style scoped>
 .employee-container {
-  background-color:rgb(255, 156, 204);
-  border: 2px solid rgb(192, 19, 105);
+  background-color:rgb(254, 192, 222);
+  border: 2px solid rgb(218, 54, 136);
   border-radius: 10px;
   padding: 20px;
   margin: 20px 0;
@@ -27,11 +24,12 @@ export default {
 }
 
 .employee-title {
-  color:rgb(227, 13, 120);
+  color:rgb(213, 0, 107);
 }
 
-.employee-name {
-  font-size: 18px;
+.employee-name, .employee-surname, .employee-age {
+  font-size: 16px;
   font-weight: bold;
+  color:rgb(244, 31, 134);
 }
 </style>
